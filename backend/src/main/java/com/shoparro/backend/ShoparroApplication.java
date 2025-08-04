76f43dff6aa2e33ecfd5e234966691f41a2dee46
Fengqi Jia <fjia0419@uni.sydney.edu.au>
2025-08-05 01:58:00 +1000

    backend set up

---
+++ backend/src/main/java/com/shoparro/backend/ShoparroApplication.java
@@ -0,0 +1,13 @@
+package com.shoparro.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoparroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoparroApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/shoparro/backend/ShoparroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoparro.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoparroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
