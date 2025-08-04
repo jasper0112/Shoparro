789e99154f230a76d5506e83ae498c9a5a3f8dad
Fengqi Jia <fjia0419@uni.sydney.edu.au>
2025-08-05 03:04:54 +1000

    update

--- backend/src/main/java/com/shoparro/backend/ShoparroApplication.java
+++ backend/src/main/java/com/shoparro/backend/ShoparroApplication.java
@@ -3,6 +3,7 @@ package com.shoparro.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ShoparroApplication {
 
